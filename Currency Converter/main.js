76e30305bd8b4aa04dfaa39784a5390754b9c9fc
Ahmed Mabrouk Said <said66913@gmail.com>
2023-02-19 18:43:51 +0200

    update file

--- Currency Converter/main.js
+++ Currency Converter/main.js
@@ -1,11 +1,11 @@
 function convert(number,type) {
     let num = Number.parseFloat(number);
     if (type === 1) {
-        let newNum = num*30.50;
+        let newNum = num / 30.50;
         document.getElementById("b").value = newNum;
     }
     else {
-        let newNum = num / 30.50;
+        let newNum = num * 30.50;
         document.getElementById("a").value = newNum;
     }
 }
