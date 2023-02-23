56b89c3d605759c63a44094b467a178e16e2e114
Ahmed Mabrouk Said <said66913@gmail.com>
2023-02-23 18:30:04 +0200

    add FAQ files

---
+++ 03 - FAQ accordion/main.js
@@ -0,0 +1,8 @@
+let items = document.querySelectorAll(".container .content ul .box");
+function makeActive() {
+    this.classList.toggle("active");
+}
+
+for (let i = 0 ; i < items.length; ++i) {
+    items[i].addEventListener("click",makeActive);
+}
