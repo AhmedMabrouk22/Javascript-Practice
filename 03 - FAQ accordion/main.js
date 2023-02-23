51089d8e4aed338a9908f5322ea529b56b16827d
Ahmed Mabrouk Said <105928025+AhmedMabrouk22@users.noreply.github.com>
2023-02-23 18:25:56 +0200

    Delete 03 - FAQ accordion directory

--- 03 - FAQ accordion/main.js
+++
@@ -1,8 +0,0 @@
-let items = document.querySelectorAll(".container .content ul .box");
-function makeActive() {
-    this.classList.toggle("active");
-}
-
-for (let i = 0 ; i < items.length; ++i) {
-    items[i].addEventListener("click",makeActive);
-}
