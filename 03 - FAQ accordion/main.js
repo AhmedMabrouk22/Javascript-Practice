4fd2586131f9350323cab6e1f997322a410b2fbf
Ahmed Mabrouk Said <said66913@gmail.com>
2023-02-23 18:00:16 +0200

    update files

--- 03 - FAQ accordion/main.js
+++ 03 - FAQ accordion/main.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 let items = document.querySelectorAll(".container .content ul .box");
 function makeActive() {
     this.classList.toggle("active");
@@ -7,15 +6,3 @@ function makeActive() {
 for (let i = 0 ; i < items.length; ++i) {
     items[i].addEventListener("click",makeActive);
 }
-
-=======
-let items = document.querySelectorAll(".container .content ul .box");
-function makeActive() {
-    this.classList.toggle("active");
-}
-
-for (let i = 0 ; i < items.length; ++i) {
-    items[i].addEventListener("click",makeActive);
-}
-
->>>>>>> 83548a42b3e7bc42db46f76bb8aea87fd3ce7df4
